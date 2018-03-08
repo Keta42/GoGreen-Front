83b951ef5c3ca35a55a775beddd52032bbb7790b
IkaTheFox <pikafeu.sm@gmail.com>
2018-03-08 15:30:37 +0100

    Homogeneization
    Linked the pages, fixed a navbar display
    Need to create a Search.php file
    Need to link with the backend
    Need to redesign

--- GoGreen-Front/Create.php
+++ GoGreen-Front/Create.php
@@ -1,10 +1,5 @@
 <!doctype html>
 <?php
-    session_start();
-    $_SESSION['message'] = '';
-    $mysqli = new mysqli("localhost", "root", "mypass123", "accounts_complete");
-
-    require 'verify.php';
     require 'nav.php';
 ?>
 
@@ -15,14 +10,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
+   
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css"/>
         <link rel="stylesheet" href="form.css" />
     <title>Alexandre Dupont</title>
   </head>
   <body>
   <?php include ('nav.php');?>
-  
+  <br><br><br>
   <form class="form-horizontal newBid" id="newBid" role="form" action="createBid.php" method="POST">
             
         
@@ -35,7 +30,7 @@
 	<b>Voiture : </b> Renault Twingo<br>
 	<input type="hidden" value="Renault Twingo"></input>
 	<label for="departure">Heure de départ :</label>
-            <input type="time" id="departure" name="departure"><br>
+            <input type="time" id="departure" name="departure"> </input><br>
 	<label for="way"> D'où : </label>
 	<select name="way">
     <option value="aller">De chez moi</option>
@@ -47,12 +42,7 @@
 	
 	
 	
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-  </body>
+      </body>
 </html>
 
 
